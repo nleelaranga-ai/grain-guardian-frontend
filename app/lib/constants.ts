11adb6e1314d313006e16ffd7371ec1c06b6e981
LEELA RANGA PRASAD <n.leelaranga@gmail.com>
2026-06-21 11:02:40 +0530

    Create constants.ts

---
+++ app/lib/constants.ts
@@ -0,0 +1,23 @@
+export const CROPS = [
+  {
+    id: 0,
+    name: "Paddy",
+    safeLimit: 13.5,
+    msp: 23.2,
+    criticalTemp: 42
+  },
+  {
+    id: 1,
+    name: "Wheat",
+    safeLimit: 13,
+    msp: 22.75,
+    criticalTemp: 40
+  },
+  {
+    id: 2,
+    name: "Maize",
+    safeLimit: 13.5,
+    msp: 20.9,
+    criticalTemp: 41
+  }
+];
