6897c17100f22c3a6e19535dd69d3ad5249f38d4
LEELA RANGA PRASAD <n.leelaranga@gmail.com>
2026-06-21 11:05:00 +0530

    Add files via upload

---
+++ app/types/history.ts
@@ -0,0 +1,7 @@
+export interface HistoricLog {
+  id: number;
+  date: string;
+  crop: string;
+  score: number;
+  lossInr: number;
+}
---
+++ app/types/telemetry.ts
@@ -0,0 +1,7 @@
+export interface SensorPayload {
+  tempT1: number;
+  tempT2: number;
+  tempT3: number;
+  humidity: number;
+  moisture: number;
+}
